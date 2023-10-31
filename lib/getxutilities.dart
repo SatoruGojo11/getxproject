ed7e533dc6f2a7a1576ff6ffca0c93e1f93fe749
Jaydip <jaydipshinga12@gmail.com>
2023-10-31 23:07:22 +0530

    Commit

---
+++ lib/getxutilities.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class GetxUtilities extends StatefulWidget {
+  const GetxUtilities({super.key});
+
+  @override
+  State<GetxUtilities> createState() => _GetxUtilitiesState();
+}
+
+class _GetxUtilitiesState extends State<GetxUtilities> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'GetX Tutorials',
+        ),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(25),
+            elevation: 10,
+            child: ListTile(
+              title: Text('Getx Alert/Simple Dialog'),
+              subtitle: Text('This is your Chat'),
+              onTap: () {
+                Get.defaultDialog(
+                  title: 'Delete',
+                  titlePadding: EdgeInsets.only(top: 20),
+                  contentPadding: EdgeInsets.all(20),
+                  // middleText: 'Are you sure?',
+                  textConfirm: 'Yes',
+                  confirmTextColor: Colors.black,
+                  textCancel: 'No',
+                  cancelTextColor: Colors.black,
+                  // content: Column(
+                  //   children: [
+                  //     Text('User 1'),
+                  //     Text('User 1'),
+                  //     Text('User 1'),
+                  //     Text('User 1'),
+                  //     Text('User 1'),
+                  //     Text('User 1'),
+                  //   ],
+                  // ),
+                );
+              },
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(25),
+            elevation: 10,
+            child: ListTile(
+              title: Text('Getx Bottom Sheet'),
+              subtitle: Text('This is your Chat'),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text('Light Theme'),
+                          leading: Icon(Icons.light_mode),
+                          onTap: () {
+                            print('light mode');
+                            Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Dark Theme'),
+                          leading: Icon(Icons.dark_mode),
+                          onTap: () {
+                            print('dark mode');
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Getx Snackbar
+          Get.snackbar(
+            'Jaydip Shingala',
+            'This is Getx Snackbar!!',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.amber,
+            icon: Icon(
+              Icons.account_circle,
+              size: 40,
+            ),
+          );
+          // Normal Snackbar
+          // ScaffoldMessenger.of(context).showSnackBar(
+          //   SnackBar(
+          //     content: Text('Jaydip Shingala'),
+          //     backgroundColor: Colors.red,
+          //     dismissDirection: DismissDirection.startToEnd,
+          //   ),
+          // );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:getxproject/home_screen.dart';
+import 'package:get/get.dart';
+import 'package:getxproject/getxutilities.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: GetxUtilities(),
     ),
   );
 }
